549b84c9b7a35ee7f4d1310918609d6fa5475363
peeryo <40718018+peeryo@users.noreply.github.com>
2023-11-13 21:08:51 -0800

    short jumps solved
    the else statement needed to align with the original if, not the 2nd if

--- player.gd
+++ player.gd
@@ -28,9 +28,9 @@ func handle_jump():
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = JUMP_VELOCITY
-		else:
-			if Input.is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY / 2:
-				velocity.y = JUMP_VELOCITY / 2 	
+	else:
+		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2 	
 				
 func handle_acceleration(input_axis, delta):
 	if input_axis != 0:
