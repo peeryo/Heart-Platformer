92a540e8a13aad14e9e1d338a3dadb747935b14c
[peeryo] <peeryo@sfusd.edu>
2024-01-02 20:40:11 -0800

    Fixed the unlimited double jumps

--- player.gd
+++ player.gd
@@ -5,11 +5,13 @@ extends CharacterBody2D
 var air_jump = false
 var just_wall_jumped = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-#var was_wall_normal = Vector2.ZERO
+var was_wall_normal = Vector2.ZERO
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 @onready var starting_position = global_position
+#not sure why my wall jump timer disappeared
+@onready var wall_jump_timer = $WallJumpTimer
 
 
 func _physics_process(delta):
@@ -21,37 +23,46 @@ func _physics_process(delta):
 	handle_air_acceleration(input_axis, delta)	
 	apply_friction(input_axis, delta)
 	apply_air_resistance(input_axis, delta)
-	#this should move to bottom of physics process
-	update_animations(input_axis)
 	var was_on_floor = is_on_floor()
+	var was_on_wall = is_on_wall_only()
+	if was_on_wall:
+		was_wall_normal = get_wall_normal()
 	move_and_slide()
 	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
 	if just_left_ledge:
 		coyote_jump_timer.start()
 	just_wall_jumped = false
+	var just_left_wall = was_on_wall and not is_on_wall()
+	if just_left_wall:
+		wall_jump_timer.start()
+	update_animations(input_axis)
 	
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * movement_data.gravity_scale * delta
 		
 func handle_wall_jump():
-	if not is_on_wall_only(): return
+	if not is_on_wall_only() and wall_jump_timer.time_left <= 0.0: return
 	var wall_normal = get_wall_normal()
+	if wall_jump_timer.time_left > 0.0:
+		wall_normal = was_wall_normal
 	if Input.is_action_just_pressed("ui_up"):
 		velocity.x = wall_normal.x * movement_data.speed
 		velocity.y = movement_data.jump_velocity
 		just_wall_jumped = true
+
 		
 func handle_jump():
 	if is_on_floor(): air_jump = true
 	
-	if is_on_floor or coyote_jump_timer.time_left > 0.0:
-		if Input.is_action_just_pressed("ui_up"):
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
+		if Input.is_action_pressed("ui_up"):
 			velocity.y = movement_data.jump_velocity
+			coyote_jump_timer.stop()
 	elif not is_on_floor():
 		if Input.is_action_just_released("ui_up") and velocity.y < movement_data.jump_velocity / 2:
 			velocity.y = movement_data.jump_velocity / 2
-			
+		
 		if Input.is_action_just_pressed("ui_up") and air_jump and not just_wall_jumped:
 			velocity.y = movement_data.jump_velocity * 0.8
 			air_jump = false
